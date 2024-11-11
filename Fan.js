11c5ef779dd80a93f6d6e3e976593ad6b6901308
Akvishwakarma1998 <Akvishwakarma.5020@gmail.com>
2024-11-11 17:43:36 +0530

    Add files via upload

---
+++ Fan.js
@@ -0,0 +1,21 @@
+let adi = document.getElementById("img")
+function myfunON()
+{
+    adi.style.animationDuration = 3+"s";
+}
+function myfunOFF()
+{
+    adi.style.animationDuration = 0+"s";
+}
+function myfun1()
+{
+    adi.style.animationDuration = 1+"s";
+}
+function myfun2()
+{
+    adi.style.animationDuration = .5+"s";
+}
+function myfun3()
+{
+    adi.style.animationDuration = .25+"s";
+}
